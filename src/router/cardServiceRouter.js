f823bbfb62b6567f4db7f5dc11ca87572029cdb2
Paul Bögelsack <>
2020-09-25 10:21:03 +0200

    Added endpoint for cash quantities

--- src/router/cardServiceRouter.js
+++ src/router/cardServiceRouter.js
@@ -1,5 +1,6 @@
 function CardServiceRouter(router, csCtrl) {
   router.route('/cardservice/transaction').post(csCtrl.SaveTransaction);
+  router.route('/cardservice/cash-quantites').post(csCtrl.SaveCashQuantities);
 
   return router;
 }

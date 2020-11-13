9640e5a925e7ba41f451a78fe8f12a1023346de9
Paul Bögelsack <>
2020-11-13 12:51:56 +0100

    changed dependencies

--- src/girowebServiceFactory.js
+++ src/girowebServiceFactory.js
@@ -1,13 +1,21 @@
 const TestService = require('./services/testService');
 const HelloTessService = require('./services/helloTessService');
+const TransactionRepository = require('./repositories/transactionRepository');
+const CashQuantityRepository = require('./repositories/cashQuantityRepository');
+const BillStockRepository = require('./repositories/billStockRepository');
 
 function GirowebServiceFactory(dbConfig) {
+  const transRepo = TransactionRepository(dbConfig);
+  const cashQuantityRepo = CashQuantityRepository(dbConfig);
+  const billStockRepo = BillStockRepository(dbConfig);
+
   function CreateService(key) {
+    const params = {transRepo, cashQuantityRepo, billStockRepo}
     switch (key) {
       case 'HelloTess':
-        return new HelloTessService(dbConfig);
+        return new HelloTessService(params);
       case 'Test':
-        return new TestService(dbConfig);
+        return new TestService(params);
       default:
         return null;
     }

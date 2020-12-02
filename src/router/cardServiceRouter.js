1e05553e9c7349f45c4d8cc79bacdb7049524945
Paul Bögelsack <>
2020-12-02 14:04:48 +0100

    neue route für bill taken hinzugefügt

--- src/router/cardServiceRouter.js
+++ src/router/cardServiceRouter.js
@@ -1,5 +1,6 @@
 function CardServiceRouter(router, csCtrl) {
   router.route('/cardservice/transaction').post(csCtrl.SaveTransaction);
+  router.route('/cardservice/bill-taken').post(csCtrl.SaveBillTaking);
   router.route('/cardservice/cash-quantities').post(csCtrl.SendCashQuantities);
 
   return router;

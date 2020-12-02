4d110fcfd792c4e6b876311cc74479cfa165d7fa
Paul Bögelsack <>
2020-12-02 14:01:42 +0100

    billTakingRepository als Abhängigkeit hinzugefügt

--- src/girowebServiceFactory.js
+++ src/girowebServiceFactory.js
@@ -3,14 +3,16 @@ const HelloTessService = require('./services/helloTessService');
 const TransactionRepository = require('./repositories/transactionRepository');
 const BillStockRepository = require('./repositories/billStockRepository');
 const BillAssumtionRepository = require('./repositories/billAssumtionRepository');
+const BillTakingRepository = require('./repositories/billTakingRepository');
 
 function GirowebServiceFactory(dbConfig) {
   
   function CreateService(key) {
     const transRepo = TransactionRepository(dbConfig);
     const billStockRepo = BillStockRepository(dbConfig);
+    const billTakingRepo = BillTakingRepository(dbConfig);
     const billAssumtionRepo = BillAssumtionRepository(dbConfig);
-    const params = {transRepo, billStockRepo, billAssumtionRepo}
+    const params = {transRepo, billStockRepo, billAssumtionRepo, billTakingRepo}
     switch (key) {
       case 'HelloTess':
         return new HelloTessService(params);

0495d38cf1f6688f797b1b4c5418ff6ab2b182c2
PaulBoegel <pboegelsack@gmail.com>
2021-02-01 21:10:45 +0100

    Added test suites for card service controller and hellotess service

--- test/helloTess/helloTessServiceTest.js
+++ test/helloTess/helloTessServiceTest.js
@@ -1,27 +1,58 @@
-const assert = require('assert');
+const sinon = require('sinon');
 const chai = require('chai');
-const expect = chai.expect;
 const HelloTessService = require('../../src/services/helloTessService');
+const cashQuantity = require('../mocks/cashQuantityMock.json');
+const http = require('http');
+const https = require('https');
+const { stub } = require('sinon');
+const expect = chai.expect;
 chai.use(require('chai-as-promised'));
 
 describe('HelloTessService SendCashQuantities', () => {
-  it('should return Error 500 with empty dependencies', async () => {
-    const transRepo = {};
-    const billStockRepo = {};
-    const billAssumtionRepo = {};
-    const billTakingRepo = {};
+  it('should use the right send protocol', async () => {
+    const transRepo = {
+      connect() {},
+      get() {
+        return [];
+      },
+    };
+    const billAssumtionRepo = {
+      connect() {},
+      get() {
+        return [];
+      },
+    };
 
     const service = HelloTessService({
       transRepo,
-      billStockRepo,
       billAssumtionRepo,
-      billTakingRepo,
     });
 
-    try {
-      await service.SendCashQuantities({});
-    } catch (error) {
-      error.should.be.Error();
-    }
+    const testService = Object.assign(service, {
+      SaveBillStock() {
+        return 200;
+      },
+      PrepareBillAssumtion() {
+        return {};
+      },
+    });
+
+    let resStub;
+    resHttp = http.ServerResponse;
+    resHttp.statusCode = 200;
+    resStub = stub(resHttp, 'on').yields('HTTPS');
+    stub(https, 'request').yields(resHttp);
+
+    process.env.HELLO_TESS_HTTPS = 'true';
+    let result = await testService.SendCashQuantities(cashQuantity);
+    expect(result).to.be.equal('HTTPS');
+
+    resStub.restore();
+    resStub = stub(resHttp, 'on').yields('HTTP');
+    stub(http, 'request').yields(resHttp);
+
+    process.env.HELLO_TESS_HTTPS = 'false';
+    result = await testService.SendCashQuantities(cashQuantity);
+    expect(result).to.be.equal('HTTP');
   });
 });

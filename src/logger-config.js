c39f3b4d241746f062e526dc29241fd8980f74f6
PaulBoegel <pboegelsack@gmail.com>
2021-04-28 12:45:30 +0200

    added logger-config

---
+++ src/logger-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  file: {
+    filename: `${appRoot}/log/service.log`,
+    timestamp: true,
+    handleExceptions: false,
+    json: true,
+    maxsize: 5242880, // 5MB
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: true,
+    colorize: true,
+  },
+  dateFormat: 'YYYY-MM-DD HH:mm:ss',
+};

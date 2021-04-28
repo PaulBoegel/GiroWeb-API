8a85c96fa197766d820e3b3c9532cf585a1f7d5f
PaulBoegel <pboegelsack@gmail.com>
2021-04-28 11:48:58 +0200

    deleted test folder

--- test/cardServiceControllerTest.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const expect = chai.expect;
-const CardServiceController = require('../../src/controller/cardServiceController');
-chai.use(require('chai-as-promised'));
-
-describe('HelloTessService SaveTransaction', () => {
-  let serviceFactory;
-  it('should return an Error if dependencies are empty', async () => {});
-});

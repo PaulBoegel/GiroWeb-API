a8b7b2391a00e32d9edfce8f0aeb47a91486339d
Paul Bögelsack <>
2021-04-29 13:10:38 +0200

    deleted file for rest api and moved functionality in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const loggerConfig = require('./logger-config');
 const logger = require('./logger/createWinston')(loggerConfig);
-const GirowebRestAPI = require('./girowebRestAPI');
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const GirowebServiceFactory = require('./girowebServiceFactory');
+const CardServiceRouter = require('./router/cardServiceRouter');
+const AuthenticationRouter = require('./router/authenticationRouter');
+const CardServiceController = require('./controller/cardServiceController');
+const AuthenticationController = require('./controller/authenticationController');
+const AuthRepository = require('./repositories/authRepository');
+const AuthMiddleware = require('./middleware/authMiddleware');
 
 process.on('uncaughtException', (err) => {
   logger.error(err.stack, () => {
@@ -9,6 +19,37 @@ process.on('uncaughtException', (err) => {
   });
 });
 
-const gwRestAPI = new GirowebRestAPI();
+const app = express();
+const port = process.env.PORT || 3000;
 
-gwRestAPI.Start();
+const serviceFactory = new GirowebServiceFactory();
+const authRepository = AuthRepository();
+const authMiddleware = AuthMiddleware(jwt);
+
+app.use(cors());
+app.use(bodyParser.raw());
+app.use(bodyParser.json());
+
+const authenticationController = new AuthenticationController({
+  jwt,
+  authRepository,
+});
+const authenticationRouter = new AuthenticationRouter(
+  express.Router(),
+  authenticationController
+);
+
+app.use('/api', authenticationRouter);
+
+const cardServiceController = new CardServiceController(serviceFactory);
+const cardServiceRouter = new CardServiceRouter(
+  express.Router(),
+  authMiddleware,
+  cardServiceController
+);
+
+app.use('/api', cardServiceRouter);
+
+app.listen(port, () => {
+  console.log(`GiroWeb Rest API listening on Port ${port}`);
+});
